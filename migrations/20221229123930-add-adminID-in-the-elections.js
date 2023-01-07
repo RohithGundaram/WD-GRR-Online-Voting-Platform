3ca465305d02fa0dfb536dc4bb9c59a11f4dedcb
Hemananda Reddy <87968757+RohithGundaram@users.noreply.github.com>
2023-01-07 22:50:36 +0530

    added all the foriegn keys

--- migrations/20221229123930-add-adminID-in-the-elections.js
+++ migrations/20221229123930-add-adminID-in-the-elections.js
@@ -9,14 +9,21 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn("Elections","adminID",{
+      type: Sequelize.DataTypes.INTEGER,
+    });
+
+    await queryInterface.addConstraint("Elections",{
+      fields: ["adminID"],
+      type: "foreign key",
+      references: {
+        table: "Admins",
+        field: "id",
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn("Elections","adminID");
   }
 };
